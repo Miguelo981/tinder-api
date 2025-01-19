95a91040860322afbcb0a1ce1d87f760deceb5bb
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2025-01-19 17:59:18 +0000

    Update API interface

--- src/interfaces/tinder.ts
+++ src/interfaces/tinder.ts
@@ -2,12 +2,16 @@ import type { TinderSearchParams, TinderSearchResponse } from "@/interfaces/sear
 import type { TinderLikeParams, TinderLikeResponse } from "@/interfaces/like.ts";
 import type { TinderDislikeParams, TinderDislikeResponse } from "@/interfaces/dislike.ts";
 import type { TinderProfileParams, TinderProfileResponse } from "@/interfaces/profile.ts";
+import type { TinderMatchesParams, TinderMatchesResponse } from "@/interfaces/matches.ts";
+import type { TinderSendMessageParams, TinderSendMessaResponse, TinderChatMessagesParams, TinderChatMessagesResponse } from "@/interfaces/chat.ts";
 import type { TinderResponse } from "@/types.ts";
-
 
 export interface ITinderAPI {
     search(params?: TinderSearchParams): Promise<TinderResponse<TinderSearchResponse>>;
     like(params?: TinderLikeParams): Promise<TinderResponse<TinderLikeResponse>>;
     dislike(params?: TinderDislikeParams): Promise<TinderResponse<TinderDislikeResponse>>;
     profile(params?: TinderProfileParams): Promise<TinderResponse<TinderProfileResponse>>
+    getMatches(params?: TinderMatchesParams): Promise<TinderResponse<TinderMatchesResponse>>
+    getChatMessages(params?: TinderChatMessagesParams): Promise<TinderResponse<TinderChatMessagesResponse>>
+    sendChatMessage(params?: TinderSendMessageParams): Promise<TinderResponse<TinderSendMessaResponse>>
 }
