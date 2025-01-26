421581b71d34605e194f2416d94b244d660afcc4
Ian M <44310933+mnai01@users.noreply.github.com>
2025-01-25 21:27:37 -0500

    formatting

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,30 +1,30 @@
-import { build, emptyDir } from "@deno/dnt";
+import { build, emptyDir } from '@deno/dnt';
 
-await emptyDir("./npm");
+await emptyDir('./npm');
 
 await build({
-  entryPoints: ["./index.ts"],
-  outDir: "./npm",
-  shims: {
-    deno: true,
-  },
-  importMap: 'deno.json',
-  package: {
-    name: "tinder-api",
-    version: Deno.args[0],
-    description: "Unofficial Tinder API wrapper for Nodejs and Deno.",
-    keywords: ['api', 'nodejs', 'tinder', 'mrbeast', 'deno', 'tinder-api'],
-    license: "MIT",
-    repository: {
-      type: "git",
-      url: "git+https://github.com/Miguelo981/tinder-api.git",
+    entryPoints: ['./index.ts'],
+    outDir: './npm',
+    shims: {
+        deno: true,
     },
-    bugs: {
-      url: "https://github.com/Miguelo981/tinder-api/issues",
+    importMap: 'deno.json',
+    package: {
+        name: 'tinder-api',
+        version: Deno.args[0],
+        description: 'Unofficial Tinder API wrapper for Nodejs and Deno.',
+        keywords: ['api', 'nodejs', 'tinder', 'mrbeast', 'deno', 'tinder-api'],
+        license: 'MIT',
+        repository: {
+            type: 'git',
+            url: 'git+https://github.com/Miguelo981/tinder-api.git',
+        },
+        bugs: {
+            url: 'https://github.com/Miguelo981/tinder-api/issues',
+        },
     },
-  },
-  postBuild() {
-    Deno.copyFileSync("LICENSE", "npm/LICENSE");
-    Deno.copyFileSync("README.md", "npm/README.md");
-  },
+    postBuild() {
+        Deno.copyFileSync('LICENSE', 'npm/LICENSE');
+        Deno.copyFileSync('README.md', 'npm/README.md');
+    },
 });
