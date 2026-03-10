213ba890e006d8ae9ed0fc424fdbc887f8975872
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2026-03-10 16:04:15 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -17,9 +17,10 @@ export const TINDER_ROUTER: Record<TinderRoute, Endpoint> = {
     chatMessages: `${API_V}/matches`,
     sendMessage: '/user/matches',
     location: `${API_V}/meta`,
-};
+} as const
 
 export const DEFAULT_LOCALE: Locale = 'es-ES';
 export const DEFAULT_SCOPES: ProfileScope[] = ['account', 'available_descriptors', 'boost', 'bouncerbypass', 'contact_cards', 'email_settings', 'feature_access', 'instagram', 'likes', 'profile_meter', 'notifications', 'misc_merchandising', 'offerings', 'onboarding', 'paywalls', 'plus_control', 'purchase', 'readreceipts', 'spotify', 'super_likes', 'tinder_u', 'travel', 'tutorials', 'user', 'all_in_gender']
 export const DEFAULT_MATCHES_COUNT = 60;
 export const DEFAULT_CHAT_MESSAGES_COUNT = 100;
+export const DEFAULT_DUOS = 0;
